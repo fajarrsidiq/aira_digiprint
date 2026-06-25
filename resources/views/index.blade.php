fc8a4878e19d5f3795dd61dccda75772417581de
fajarrsidiq <fsidiq596@gmail.com>
2026-06-25 10:46:22 +0700

    [Perbaikan] pemanggilan style taillwind css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AIRA Advertising - Solusi Percetakan Digital</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <style>
         .bg-aira-red { background-color: #DC2626; }
         .text-aira-red { color: #DC2626; }
