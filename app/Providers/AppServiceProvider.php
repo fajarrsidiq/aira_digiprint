1d77e5a73238de74d6dfa33799e3ddfd25998b38
fajarrsidiq <fsidiq596@gmail.com>
2026-06-27 12:18:44 +0700

    a

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,23 +4,22 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\URL;
 use App\Models\Transaksi;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         View::composer('layouts.app', function ($view) {
             $notifCount = Transaksi::where('status_pesanan', 'Menunggu Konfirmasi')->count();
             $view->with('notifCount', $notifCount);
